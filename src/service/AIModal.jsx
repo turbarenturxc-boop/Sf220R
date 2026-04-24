48ae0219c3390904332691252b598a7bb6e8add2
Sonekraros <turbarenturxc@gmail.com>
2026-04-24 08:50:12 +0700

    pre-done5

--- src/service/AIModal.jsx
+++ src/service/AIModal.jsx
@@ -4,7 +4,7 @@ const apiKey = import.meta.env.VITE_GOOGLE_GEMINI_AI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-3-flash-preview",
+  model: "gemini-2.5-flash",
 });
 
 const generationConfig = {
