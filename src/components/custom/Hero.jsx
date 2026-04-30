0efde1d89289a2424f8205e20a0a49a90b23df06
Sonekraros <turbarenturxc@gmail.com>
2026-04-30 20:37:04 +0700

    remove react globe from hero

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,28 +1,16 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import { Button } from '../ui/button'
 import { Link } from 'react-router-dom'
-import Globe from 'react-globe.gl'
 
 function Hero() {
-  const globeRef = useRef()
-
-  useEffect(() => {
-    if (globeRef.current) {
-      globeRef.current.controls().autoRotate = true
-      globeRef.current.controls().autoRotateSpeed = 0.6
-    }
-  }, [])
-
   return (
     <div className="relative min-h-screen overflow-hidden text-white">
-
       <div className="absolute inset-0 bg-[url('https://images.pexels.com/photos/9665177/pexels-photo-9665177.jpeg')] bg-cover bg-center"></div>
 
       <div className="absolute inset-0 bg-black/70"></div>
 
       {/* Content */}
-      <div className="relative z-10 flex flex-col items-center px-6 pt-20 gap-8">
-
+      <div className="relative z-10 flex min-h-screen flex-col items-center justify-center px-6 gap-8">
         <h1 className="text-[42px] md:text-[56px] font-extrabold text-center max-w-5xl">
           <span className="text-blue-500">
             Plan Your Dream Trip
@@ -39,20 +27,6 @@ function Hero() {
             Create my trip
           </Button>
         </Link>
-
-        {/* 🌍 Globe */}
-        <div className="relative z-10 flex justify-center items-center w-full overflow-hidden">
-          <div className="w-[700px] h-[700px] flex justify-center items-center">
-            <Globe
-              ref={globeRef}
-              globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
-              backgroundColor="rgba(0,0,0,0)"
-              width={700}
-              height={700}
-            />
-          </div>
-        </div>
-
       </div>
     </div>
   )
